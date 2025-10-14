59df24d91f304383535a43757ef88e5cda7e861d
joao9417 <orozcojoan72@gmail.com>
2025-10-13 21:46:17 -0500

    feat: calculo luminarias en proceso, llamado de base de datos realizado

--- m1_carga_termica/urls.py
+++ m1_carga_termica/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('calculo/', views.calculo_carga_termica, name='calculo_carga_termica'),
     path('calculo/<str:nombre_cuarto>/', views.calculo_carga_termica, name='calculo_carga_termica_nombre'),
     path('api/evaporador-data/', views.obtener_datos_evaporador, name='obtener_datos_evaporador'),
-    path('api/productos-data/', views.obtener_datos_producto, name='obtener_datos_evaporador'),
+    path('api/productos-data/', views.obtener_datos_producto, name='obtener_datos_producto'),
+    path('api/lampara-data/', views.obtener_datos_lampara_led, name='obtener_datos_lampara'),
 ]
